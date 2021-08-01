995831e8ca4cf3a344b56708306b3f4284e67ae1
imanvirsingh <62115231+imanvirsingh@users.noreply.github.com>
2021-08-01 05:09:17 -0400

    created Nutrients.java

---
+++ app/src/main/java/com/tanvi/healthpal/ui/scanqr/apis/entity/elements/Nutrients.java
@@ -0,0 +1,112 @@
+package com.tanvi.healthpal.ui.scanqr.apis.entity.elements;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Nutrients {
+    private Double calcium;
+    private Double calcium_100g;
+    private Double calcium_serving;
+    private String calcium_unit;
+    private Double calcium_value;
+    private Double carbohydrates;
+    private Double carbohydrates_100g;
+    private Double carbohydrates_serving;
+    private String carbohydrates_unit;
+    private Double carbohydrates_value;
+    private Double cholesterol;
+    private Double cholesterol_100g;
+    private Double cholesterol_serving;
+    private String cholesterol_unit;
+    private Double cholesterol_value;
+    private Double energy;
+    private Double energy_kcal;
+    @SerializedName("energy-kcal_100g")
+    private Double energy_kcal_100g;
+    @SerializedName("energy-kcal_serving")
+    private Double energy_kcal_serving;
+    @SerializedName("energy-kcal_unit")
+    private String energy_kcal_unit;
+    @SerializedName("energy-kcal_value")
+    private Double energy_kcal_value;
+    private Double energy_100g;
+    private Double energy_serving;
+    private String energy_unit;
+    private Double energy_value;
+    private Double fat;
+    private Double fat_100g;
+    private Double fat_serving;
+    private String fat_unit;
+    private Double fat_value;
+    private Double fiber;
+    private Double fiber_100g;
+    private Double fiber_serving;
+    private String fiber_unit;
+    private Double fiber_value;
+    private Double fruits_vegetables_nuts_estimate_from_ingredients_100g;
+    private Double iron;
+    private Double iron_100;
+    private Double iron_serving;
+    private String iron_unit;
+    private Double iron_value;
+    private Double nova_group;
+    private Double nova_group_100g;
+    private Double nova_group_serving;
+    private Double nutrition_score_fr;
+    private Double nutrition_score_fr_100g;
+    private Double proteins;
+    private Double proteins_100g;
+    private Double proteins_serving;
+    private String proteins_unit;
+    private Double proteins_value;
+    private Double salt;
+    private Double salt_100g;
+    private Double salt_serving;
+    private String salt_unit;
+    private Double salt_value;
+    private Double saturated_fat;
+    @SerializedName("saturated-fat_100g")
+    private Double saturated_fat_100g;
+    @SerializedName("saturated-fat_serving")
+    private Double saturated_fat_serving;
+    @SerializedName("saturated-fat_unit")
+    private String saturated_fat_unit;
+    @SerializedName("saturated-fat_value")
+    private Double saturated_fat_value;
+    private Double sodium;
+    private Double sodium_100g;
+    private Double sodium_serving;
+    private String sodium_unit;
+    private Double sodium_value;
+    private Double sugars;
+    private Double sugars_100g;
+    private Double sugars_serving;
+    private String sugars_unit;
+    private Double sugars_value;
+    private Double trans_fat;
+    @SerializedName("trans-fat_100g")
+    private Double trans_fat_100g;
+    @SerializedName("trans-fat_serving")
+    private Double trans_fat_serving;
+    @SerializedName("trans-fat_unit")
+    private String trans_fat_unit;
+    @SerializedName("trans-fat_value")
+    private Double trans_fat_value;
+    private Double vitamin_a;
+    @SerializedName("vitamin-a_100g")
+    private Double vitamin_a_100g;
+    @SerializedName("vitamin-a_serving")
+    private Double vitamin_a_serving;
+    @SerializedName("vitamin-a_unit")
+    private String vitamin_a_unit;
+    @SerializedName("vitamin-a_value")
+    private Double vitamin_a_value;
+    private Double vitamin_c;
+    @SerializedName("vitamin-c_100g")
+    private Double vitamin_c_100g;
+    @SerializedName("vitamin-c_serving")
+    private Double vitamin_c_serving;
+    @SerializedName("vitamin-c_unit")
+    private String vitamin_c_unit;
+    @SerializedName("vitamin-c_value")
+    private Double vitamin_c_value;
+}
--- app/src/main/java/com/tanvi/healthpal/ui/scanqr/apis/entity/elements/Product.java
+++ app/src/main/java/com/tanvi/healthpal/ui/scanqr/apis/entity/elements/Product.java
@@ -1,5 +1,49 @@
 package com.tanvi.healthpal.ui.scanqr.apis.entity.elements;
 
 public class Product {
-    
+    private Nutrients nutrients;
+    private String product_name;
+    private String image_front_small_url;
+    private String image_front_thumb_url;
+    private String image_front_url;
+
+    public Nutrients getNutrients() {
+        return nutrients;
+    }
+
+    public void setNutrients(Nutrients nutrients) {
+        this.nutrients = nutrients;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
+    }
+
+    public String getImage_front_small_url() {
+        return image_front_small_url;
+    }
+
+    public void setImage_front_small_url(String image_front_small_url) {
+        this.image_front_small_url = image_front_small_url;
+    }
+
+    public String getImage_front_thumb_url() {
+        return image_front_thumb_url;
+    }
+
+    public void setImage_front_thumb_url(String image_front_thumb_url) {
+        this.image_front_thumb_url = image_front_thumb_url;
+    }
+
+    public String getImage_front_url() {
+        return image_front_url;
+    }
+
+    public void setImage_front_url(String image_front_url) {
+        this.image_front_url = image_front_url;
+    }
 }
